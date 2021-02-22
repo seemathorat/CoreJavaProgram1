822d0f7ff73394a3e1ccb8c7058d2176f34f5b0e
seema thorat <seemagade97@gmail.com>
2021-02-22 12:39:30 +0530

    employee present or Absecent

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,11 @@
-public class  HelloWorld
-
-{
-  public static void main(String args[])
-{
-
-System.out.println("Welcome message");
-
-}
+public class HelloWorld{
+	public static void main(String[] args){
+		int isFullTime=1;
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+		
+		if( empCheck == isFullTime )
+			System.out.println("Employee is present");
+		else		
+			System.out.println("Employee is absent");
+	}
 }
