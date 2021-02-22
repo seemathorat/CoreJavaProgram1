14f970e9ef52120ea990094c71b37df35c81b379
seema thorat <seemagade97@gmail.com>
2021-02-22 13:47:02 +0530

    Pushing File in part time done

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,17 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		int isFullTime=1;
+		int isPartTime=1;	
+		int isFullTime=2;
 		int empRatePerHr=20;
 	int empHrs=0;
 	int empWage=0;
-		double empCheck=Math.floor(Math.random() * 10) % 2;
+		double empCheck=Math.floor(Math.random() * 10) % 3;
 		
-		if( empCheck == isFullTime )
+		if( empCheck == isPartTime )
+			empHrs=4;
+		else if(empCheck==isFullTime)
 			empHrs=8;
+		
 		else		
 			empHrs=0;
 			empWage=empHrs * empRatePerHr;
