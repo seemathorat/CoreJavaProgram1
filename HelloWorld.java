e5e35e57d4f1da83f802bc4f64a9d90a1b43895d
seema thorat <seemagade97@gmail.com>
2021-02-22 14:10:17 +0530

    calculate Employee Wage for month

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,15 @@ public class HelloWorld{
 	 public static final int isPartTime=1;
       public static final int isFullTime=2;
       public static final int empRatePerHr=20;
-
+		public static final int numOfWorkingDays=2;
+	
 	public static void main(String[] args){
 	int empHrs=0;
 	int empWage=0;
-		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+	int totalEmpWage=0;
+		for(int day=0; day<numOfWorkingDays; day++){
+				int empCheck=(int) Math.floor(Math.random() *10) % 3;
+			
 		switch (empCheck){
 			case isPartTime:
 							empHrs=4;
@@ -19,9 +23,9 @@ public class HelloWorld{
 					empHrs=0;
 	}
 			empWage=empHrs * empRatePerHr;
+			totalEmpWage +=empWage;
 			System.out.println("Employee Wage: "+empWage);
 	}
-
-
-
+System.out.println("Total Emp Wage: " +totalEmpWage);
+}
 }
